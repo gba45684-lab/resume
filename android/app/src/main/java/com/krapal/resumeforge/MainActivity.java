4fbe49a6218f019f015d9fc6c8d8cfdc1c6c9dd1
gba45684-lab <gba45684@gmail.com>
2026-09-14 16:42:04 +0530

    Register native PDF export plugin

--- android/app/src/main/java/com/krapal/resumeforge/MainActivity.java
+++ android/app/src/main/java/com/krapal/resumeforge/MainActivity.java
@@ -2,4 +2,10 @@ package com.krapal.resumeforge;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(android.os.Bundle savedInstanceState) {
+        registerPlugin(PdfExportPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
